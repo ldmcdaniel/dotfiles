cbf177da4146ac2c99a1927cd17d75d3886d0d6e
Lucas McDaniel <lmcdaniel@myemma.com>
2018-04-28 13:57:13 -0500

    Fixed (I beleive) menu script to install resources

--- menu.sh
+++ menu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 while true; do
-  read -p "Do you want to install new resources or update existing resources (i/u)? " hb
+  read -p "Do you want to install new resources(y/n)? " hb
   case $hb in
-    [Ii]* ) sh ~/dotfiles/.scripts/install.sh;
+    [Yy]* ) sh ~/dotfiles/.scripts/install.sh;
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
       break;;
-    [Uu]* ) sh ~/dotfiles/.scripts/update.sh; break;;
-    * ) echo "Please choose install or update.";;
+    [Nn]* ) break;;
+    * ) echo "Please enter y(yes) or n(no).";;
   esac
 done
